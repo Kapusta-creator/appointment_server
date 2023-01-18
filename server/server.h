6a27d74b6b1df4b5b9ce743c18c4773a84901df4
Kapusta-creator <74546809+Kapusta-creator@users.noreply.github.com>
2023-01-18 21:51:11 +0300

    Add files via upload

---
+++ server/server.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include<Poco/Mutex.h>
+#include<Poco/Util/ServerApplication.h>
+#include<Poco/Data/Session.h>
+
+using namespace Poco;
+using namespace Poco::Util;
+
+/*!
+	\brief Основной класс сервера
+	
+	Данный класс реализует прием и отправку запросов от приложения-клиента.
+*/
+class AppointmentServer : public ServerApplication
+{
+protected:
+	int main(const std::vector<std::string>&);
+};
